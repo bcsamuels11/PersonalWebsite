49ecc8a002593087e4da316fb1da678a8951fefd
bcsamuels11 <bcsamuels11@gmail.com>
2024-02-19 00:23:48 -0500

    Updates

--- src/routes.js
+++
@@ -1,9 +0,0 @@
-// let BASE = "/PersonalWebsite";
-let HOME = "/";
-let ABOUT = "/about";
-let WORK = "/work";
-let RESEARCH = "/research";
-// let PERSONAL = "/PersonalWebsite/personal/";
-let CONTACT = "/contact";
-
-export { HOME, ABOUT, WORK, RESEARCH, CONTACT };
